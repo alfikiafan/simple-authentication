3693bb97d4a393a751e6f6e25bfd5f2a721a063e
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-05-27 21:42:00 +0700

    Add 404 page

--- includes/errors/404.php
+++ includes/errors/404.php
@@ -1,3 +1,23 @@
-<?php
-
-echo 'Kamu salah link. Periksa ulang struktur URL dengan benar.';
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Authentication System</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <script>
+      <?php require_once("../../js/main.js");?>
+    </script>
+  </head>
+  <body>
+    <section class="ftco-section">
+      <div class="container text-center mt-5">
+        <h1 class="display-1 text-white">404</h1>
+        <p class="lead text-white mb-5">The page that you have requested could not be found.</p>
+        <a href="../../index.php" class="btn btn-primary p-3">Back to homepage</a>
+      </div>
+    </section>
+  </body>
+</html>
