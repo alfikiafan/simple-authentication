e8eefaf808c1e0d111fff4d39909f1dd0d3ee625
Alfiki Diastama Afan Firdaus <alfiki@student.uns.ac.id>
2023-05-28 18:40:53 +0700

    Remove unnecessary css

--- resources.php
+++ resources.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://alfikiafan.github.io/bootstrap-custom/style.css">
+    <link rel="stylesheet" href="https://alfikiafan.github.io/bootstrap-custom/style.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
       <?php require_once("js/main.js");?>
     </script>
   </head>
-  <body>
+  <body class="background">
